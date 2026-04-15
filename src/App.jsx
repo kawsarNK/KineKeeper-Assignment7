4e3889f38d65b9a8edd77496eb4f30fcc01a0da5
kawsarNK <mdkawsarnk78@gmail.com>
2026-04-15 13:33:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <h2 className='bg-red-400'>Hello React 3</h2>
+    </>
+  )
+}
+
+export default App
