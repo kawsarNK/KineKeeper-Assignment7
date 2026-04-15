0577682eb4c556fb8deb0e821fc3609d8264f844
kawsarNK <mdkawsarnk78@gmail.com>
2026-04-15 13:47:08 +0600

    Implement navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -2,22 +2,15 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-white border-2 border-amber-50 shadow-sm">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
                     <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="bg-base-100 rounded-t-none p-2">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
+                    <li><a >Link2</a></li>
+                    <li><a >Link3</a></li>
                 </ul>
             </div>
         </div>
